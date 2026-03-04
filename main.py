f7d2544b71998c4b0272bc264723bab26d254f91
Lakshmanan Palaniappan <lakshmananletchu8@gmail.com>
2026-03-04 21:43:25 +0530

    feat: Tested Pyspark

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from pyspark.sql import SparkSession
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+spark = SparkSession.builder.appName("TestApp").getOrCreate()
+df = spark.createDataFrame([(1, "Alice"), (2, "Bob")], ["id", "name"])
+df.show()
